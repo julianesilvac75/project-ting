7992367ab1f509e8260bd01bba9d3557db9b8bde
Juliane <julianesilvac75@gmail.com>
2023-01-14 23:18:35 +0000

    feat: implementing txt importer function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação."""
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+        with open(path_file, mode="r") as file:
+            lines = [line.rstrip("\n") for line in file]
+
+        return lines
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
