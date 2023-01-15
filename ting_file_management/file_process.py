33eb4f988ffa2b4367ef8237ab9f27a1b5fd58eb
Juliane <julianesilvac75@gmail.com>
2023-01-15 16:39:24 +0000

    feat: implementing process file function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    list = txt_importer(path_file)
+
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+
+    file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(list),
+        "linhas_do_arquivo": [line for line in list]
+    }
+
+    instance.enqueue(file_dict)
+
+    print(file_dict)
 
 
 def remove(instance):
