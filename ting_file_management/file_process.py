67be121465d0243d80e681702a9393163b6124dd
Juliane <julianesilvac75@gmail.com>
2023-01-15 16:50:48 +0000

    feat: implementing remove file function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -20,7 +20,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print("Não há elementos")
+    else:
+        file = instance.search(0)
+        instance.dequeue()
+        print(f"Arquivo {file['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
