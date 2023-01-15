cd69f112b66672e8065db973c663adde9c79e217
Juliane <julianesilvac75@gmail.com>
2023-01-15 17:19:00 +0000

    refactor: process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,16 +3,16 @@ from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    list = txt_importer(path_file)
-
     for i in range(len(instance)):
         if instance.search(i)["nome_do_arquivo"] == path_file:
             return None
 
+    list = txt_importer(path_file)
+
     file_dict = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(list),
-        "linhas_do_arquivo": [line for line in list]
+        "linhas_do_arquivo": list
     }
 
     instance.enqueue(file_dict)
