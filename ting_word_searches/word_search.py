a8f10133f31ea2404ff1bd4512cb3aca479a8a46
Juliane <julianesilvac75@gmail.com>
2023-01-15 17:47:30 +0000

    feat: implementing exists word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for file in instance.data:
+        file_dict = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        for i in range(len(file["linhas_do_arquivo"])):
+            if word.lower() in file["linhas_do_arquivo"][i].lower():
+                file_dict["ocorrencias"].append({"linha": i + 1})
+
+        if len(file_dict["ocorrencias"]):
+            result.append(file_dict)
+
+    return result
 
 
 def search_by_word(word, instance):
